2a7b1298d707532e6de935abd1ee238dc439639b
Jay Kania <jaykania123@gmail.com>
2025-04-07 23:59:02 +0530

    fix: fixed scrollback buffer not getting cleared on terminal and iterm

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"log"
 	"os"
-
 	"slices"
 
 	"golang.org/x/term"
 )
 
 func clearScreen() {
-	fmt.Print("\033[3J\033[H\033[2J") // ANSI escape code to clear screen + scrollback and move cursor to top-left
+	// Sequences for clearing both screen and scrollback buffer
+	fmt.Print("\033[2J")   // Clear entire screen
+	fmt.Print("\033[3J")   // Clear scrollback buffer
+	fmt.Print("\033[H")    // Move cursor to top-left corner
 }
 
 func renderList(selected int, options []string) {
@@ -28,9 +30,7 @@ func renderList(selected int, options []string) {
 	fmt.Printf("\033[%d;1H", selected+1)
 }
 
-
 func main() {
-
 	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
 		panic(err)
@@ -38,10 +38,10 @@ func main() {
 
 	defer term.Restore(int(os.Stdin.Fd()), oldState)
 
-    buf := make([]byte, 3)
+	buf := make([]byte, 3)
 	selectedOption := 0
-	options := buildOptions();
-	renderList(selectedOption,options)
+	options := buildOptions()
+	renderList(selectedOption, options)
 	for {
 		n, err := os.Stdin.Read(buf)
 		if err != nil {
@@ -54,16 +54,18 @@ func main() {
 
 		if n == 3 && buf[0] == 0x1b && buf[1] == '[' {
 			switch buf[2] {
-				case 'A': {
-					if selectedOption == 0  {
-						selectedOption = len(options) - 1;
+			case 'A':
+				{
+					if selectedOption == 0 {
+						selectedOption = len(options) - 1
 					} else {
-						selectedOption--;
+						selectedOption--
 					}
 					renderList(selectedOption, options)
 				}
-				case 'B': {
-					if selectedOption == len(options) - 1  {
+			case 'B':
+				{
+					if selectedOption == len(options) - 1 {
 						selectedOption = 0
 					} else {
 						selectedOption++
@@ -81,12 +83,12 @@ func buildOptions() []string {
 		log.Fatal(err)
 	}
 
-	// separating directories and files as there might os.ReadDir(".") does not maintain the order 
+	// separating directories and files as os.ReadDir(".") does not maintain the order
 	dirOptions := []string{}
 	fileOptions := []string{}
 
 	for i := 0; i < len(children); i++ {
-		if(children[i].IsDir()) {
+		if children[i].IsDir() {
 			dirOptions = append(dirOptions, "/"+children[i].Name())
 		} else {
 			fileOptions = append(fileOptions, children[i].Name())
